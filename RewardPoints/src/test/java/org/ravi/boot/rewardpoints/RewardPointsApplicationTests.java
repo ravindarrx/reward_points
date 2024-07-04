4024cc55e5d1139579cd18c2fe5002aca5818382
ravthara <ravindar.tharanikumar@gmail.com>
2024-07-04 17:02:12 +0530

    Initial version

---
+++ RewardPoints/src/test/java/org/ravi/boot/rewardpoints/RewardPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ravi.boot.rewardpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardPointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
